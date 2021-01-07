daf1c5dbfd4fb1d1524411ddd4e6b0ff2f8b9808
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-01-07 18:00:45 +0100

    37. Modo Chunked para menejar la contrapresión

--- src/main/java/com/hdconsulting/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/hdconsulting/springboot/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package com.hdconsulting.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.hdconsulting.springboot.webflux.app.models.dao.ProductoDao;
 import com.hdconsulting.springboot.webflux.app.models.documents.Producto;
@@ -30,6 +33,40 @@ public class ProductoController {
 				});
 		
 		productos.subscribe(prod -> log.info(prod.getNombre()));
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+	
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		
+		Flux<Producto> productos = dao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		//ReactiveDataDriverContextVariable -> pour pas attendre que le flux soit "completed"
+		//pour etre consomme
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+	
+	
+	@GetMapping("/listar-full")
+	public String listarFull(Model model) {
+		
+		Flux<Producto> productos = dao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				}).repeat(5000);
+		
+
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
 		return "listar";
