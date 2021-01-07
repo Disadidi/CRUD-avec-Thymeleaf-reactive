90f038c25c912817ca71f155ca1f8e617714b0aa
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-01-07 18:17:29 +0100

    Modo Chunked view names

--- src/main/java/com/hdconsulting/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/hdconsulting/springboot/webflux/app/controllers/ProductoController.java
@@ -71,5 +71,20 @@ public class ProductoController {
 		model.addAttribute("titulo", "Listado de productos");
 		return "listar";
 	}
+	
+	@GetMapping("/listar-chunked")
+	public String listarChunked(Model model) {
+		
+		Flux<Producto> productos = dao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				}).repeat(5000);
+		
+
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar-chunked";
+	}
 
 }
