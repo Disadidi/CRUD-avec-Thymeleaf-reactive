5314a547854bd8ee7e8756f242a8e6e628c1e2a6
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-01-07 17:28:52 +0100

    Suscribiendo otro Observadry modificando el Stream reactivo

---
+++ src/main/java/com/hdconsulting/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,38 @@
+package com.hdconsulting.springboot.webflux.app.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hdconsulting.springboot.webflux.app.models.dao.ProductoDao;
+import com.hdconsulting.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired
+	private ProductoDao dao;
+	
+	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+	
+	@GetMapping({"/listar", "/"})
+	public String listar(Model model) {
+		
+		Flux<Producto> productos = dao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				});
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+
+}
