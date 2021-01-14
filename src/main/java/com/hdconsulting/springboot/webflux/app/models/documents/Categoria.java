7440015c4565af5c672957b78f368cc3e90aba6b
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-01-14 10:39:30 +0100

    Añadiendo relaciones de colecciones y nuevo documento categoria

---
+++ src/main/java/com/hdconsulting/springboot/webflux/app/models/documents/Categoria.java
@@ -0,0 +1,44 @@
+package com.hdconsulting.springboot.webflux.app.models.documents;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Categoria {
+	
+	@Id
+	private String id;
+	
+	private String nombre;
+	
+	
+
+	public Categoria() {
+	}
+
+	public Categoria(String nombre) {
+		
+		this.nombre = nombre;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+
+}
